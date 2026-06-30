566e5f66433261111706819cf4088b37bf322419
Muhammad Ali <118616454+MAliSohail@users.noreply.github.com>
2026-06-30 12:46:08 +0200

    changed reset style

--- 8-BIT_CPU/VHDL/Control_Unit.vhd
+++ 8-BIT_CPU/VHDL/Control_Unit.vhd
@@ -1,6 +1,5 @@
 library ieee;
 use ieee.std_logic_1164.all;
-
 use work.CPU_Package.all;
 
 entity Control_Unit is
@@ -9,9 +8,9 @@ entity Control_Unit is
         Reset        : in  std_logic;
         Clock_Enable : in  std_logic;
 
-        Opcode     : in  std_logic_vector(3 downto 0);
-        Zero_Flag  : in  std_logic;
-        Carry_Flag : in  std_logic;
+        Opcode       : in  std_logic_vector(3 downto 0);
+        Zero_Flag    : in  std_logic;
+        Carry_Flag   : in  std_logic;
 
         IR_Load      : out std_logic;
         PC_Increment : out std_logic;
@@ -20,39 +19,71 @@ entity Control_Unit is
         RAM_Write    : out std_logic;
         Output_Load  : out std_logic;
         Halt_Control : out std_logic;
+
         State_Debug  : out std_logic_vector(3 downto 0)
     );
 end entity;
 
 architecture RTL of Control_Unit is
+
     signal Current_State : CPU_State_Type := STATE_FETCH;
+    signal Next_State    : CPU_State_Type := STATE_FETCH;
+
 begin
 
-    process(Clock)
+    --------------------------------------------------------------------
+    -- State memory process
+    -- This stores the current FSM state.
+    -- Reset is asynchronous, because it is checked before the clock edge.
+    --------------------------------------------------------------------
+    state_memory : process(Clock, Reset)
     begin
-        if rising_edge(Clock) then
-            if Reset = '1' then
-                Current_State <= STATE_FETCH;
-            elsif Clock_Enable = '1' then
-                case Current_State is
-                    when STATE_FETCH =>
-                        Current_State <= STATE_DECODE;
-                    when STATE_DECODE =>
-                        Current_State <= STATE_EXECUTE;
-                    when STATE_EXECUTE =>
-                        if Opcode = OP_HALT then
-                            Current_State <= STATE_HALT;
-                        else
-                            Current_State <= STATE_FETCH;
-                        end if;
-                    when STATE_HALT =>
-                        Current_State <= STATE_HALT;
-                end case;
+        if Reset = '1' then
+            Current_State <= STATE_FETCH;
+
+        elsif rising_edge(Clock) then
+            if Clock_Enable = '1' then
+                Current_State <= Next_State;
             end if;
         end if;
-    end process;
+    end process state_memory;
 
-    process(Current_State, Opcode, Zero_Flag, Carry_Flag)
+
+    --------------------------------------------------------------------
+    -- Next-state logic
+    -- This decides which state comes next.
+    --------------------------------------------------------------------
+    next_state_logic : process(Current_State, Opcode)
+    begin
+        Next_State <= Current_State;
+
+        case Current_State is
+
+            when STATE_FETCH =>
+                Next_State <= STATE_DECODE;
+
+            when STATE_DECODE =>
+                Next_State <= STATE_EXECUTE;
+
+            when STATE_EXECUTE =>
+                if Opcode = OP_HALT then
+                    Next_State <= STATE_HALT;
+                else
+                    Next_State <= STATE_FETCH;
+                end if;
+
+            when STATE_HALT =>
+                Next_State <= STATE_HALT;
+
+        end case;
+    end process next_state_logic;
+
+
+    --------------------------------------------------------------------
+    -- Output logic
+    -- These control signals tell the other CPU blocks what to do.
+    --------------------------------------------------------------------
+    output_logic : process(Current_State, Opcode, Zero_Flag, Carry_Flag)
     begin
         IR_Load      <= '0';
         PC_Increment <= '0';
@@ -68,58 +99,75 @@ begin
             when STATE_FETCH =>
                 IR_Load      <= '1';
                 PC_Increment <= '1';
-                State_Debug  <= "0001";
+                State_Debug  <= "0001"; -- Display 1 = FETCH
 
             when STATE_DECODE =>
-                State_Debug <= "0010";
+                State_Debug  <= "0010"; -- Display 2 = DECODE
 
             when STATE_EXECUTE =>
-                State_Debug <= "0011";
+                State_Debug  <= "0011"; -- Display 3 = EXECUTE
 
                 case Opcode is
+
                     when OP_LOAD_IMMEDIATE =>
                         ACC_Load <= '1';
+
                     when OP_LOAD_MEMORY =>
                         ACC_Load <= '1';
+
                     when OP_STORE_MEMORY =>
                         RAM_Write <= '1';
+
                     when OP_ADD_MEMORY =>
                         ACC_Load <= '1';
+
                     when OP_SUB_MEMORY =>
                         ACC_Load <= '1';
+
                     when OP_AND_MEMORY =>
                         ACC_Load <= '1';
+
                     when OP_OR_MEMORY =>
                         ACC_Load <= '1';
+
                     when OP_XOR_MEMORY =>
                         ACC_Load <= '1';
+
                     when OP_NOT_ACC =>
                         ACC_Load <= '1';
+
                     when OP_JUMP =>
                         PC_Load <= '1';
+
                     when OP_JUMP_IF_ZERO =>
                         if Zero_Flag = '1' then
                             PC_Load <= '1';
                         end if;
+
                     when OP_JUMP_IF_CARRY =>
                         if Carry_Flag = '1' then
                             PC_Load <= '1';
                         end if;
+
                     when OP_OUT =>
                         Output_Load <= '1';
+
                     when OP_CLEAR =>
                         ACC_Load <= '1';
+
                     when OP_HALT =>
                         Halt_Control <= '1';
+
                     when others =>
                         null;
+
                 end case;
 
             when STATE_HALT =>
                 Halt_Control <= '1';
-                State_Debug  <= "1111";
+                State_Debug  <= "1111"; -- Display F = HALT
 
         end case;
-    end process;
+    end process output_logic;
 
 end architecture;
